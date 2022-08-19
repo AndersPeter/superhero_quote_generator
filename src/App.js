daac290fb43713a8c1a3e889f35839d1c1f7cf9b
AndersPeter <anderspeter1982@gmail.com>
2022-08-19 19:43:39 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.scss";
+import COLORS_ARRAY from "../src/colorArray.js";
+import { CgTwitter } from 'react-icons/cg';
 
 function App() {
+  const [quote, setQuote] = useState(
+    `It’s not who I am underneath, but what I do that defines me.` 
+  );
+  const [author, setAuthor] = useState("Batman");
+  const [, setRandomNumber] = useState(0);
+  const [ , setQuotesArray] = useState(null);
+  const [accentColor, setAccentColor] = useState("#282c34");
+
+  const fetchQuotes = async ( ) => {
+    const response = await fetch("https://superhero-quotes.herokuapp.com/random");
+    const parsedJSON = await response.json();
+    setQuotesArray(parsedJSON);
+    setQuote(parsedJSON.Stuff.data.quote);
+    setAuthor(parsedJSON.Stuff.data.author);
+    let randomInteger = Math.floor(COLORS_ARRAY.length * Math.random());
+    setRandomNumber(randomInteger);
+    setAccentColor(COLORS_ARRAY[randomInteger]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{ backgroundColor: accentColor }}>
+        <div id="quote-box" style={{ color: accentColor }}>
+          <p style={{fontWeight: "bold"}}> Superhero quote generator</p>
+          <p id="text">{quote}</p>
+          <p id="author">-{author}</p>
+          <div className="button">
+            <a
+              style={{ backgroundColor: accentColor }}
+              target="_top"
+              id="tweet-quote"
+              href={encodeURI(
+                `http://www.twitter.com/intent/tweet?text="${quote}" -${author}`
+              )}
+            >
+              <CgTwitter />
+            </a>
+          <button
+            style={{ backgroundColor: accentColor }}
+            id="new-quote"
+            onClick={() => fetchQuotes()}
+            >
+            New quote
+          </button>
+            </div>
+        </div>
       </header>
+      <p>Copyright 2022. Anders Peter Sørensen</p>
     </div>
   );
 }
